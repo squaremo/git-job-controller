ea1221a08379f214c3968278a5ba04e14c6eeda1
Michael Bridgen <mikeb@squaremobius.net>
2020-08-18 17:29:35 +0100

    Give GitUpdateJob a spec and a status
    I'm guessing a little at this point, especially at the protocol
    between the job and the image that's supplied to run within it. These
    fields are gesturing towards the kyaml function protocol.

--- api/v1alpha1/gitupdatejob_types.go
+++ api/v1alpha1/gitupdatejob_types.go
@@ -17,28 +17,66 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // GitUpdateJobSpec defines the desired state of GitUpdateJob
 type GitUpdateJobSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of GitUpdateJob. Edit GitUpdateJob_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// GitRepositoryRef refers to a GitRepository object giving access
+	// to the repository on which to run the job.
+	// +required
+	GitRepositoryRef corev1.LocalObjectReference `json:"gitRepositoryRef"`
+	// Image gives the image to run as a function in the git repo.
+	// +required
+	Image string `json:"image"`
+	// Args lists any arguments to be given to the image.
+	// +optional
+	Args []string `json:"args,omitempty"`
+	// FunctionConfig gives a resource to supply as the function's
+	// input.
+	// +optional
+	FunctionConfig string `json:"functionConfig,omitempty"`
 }
+
+type GitJobResult string
+
+const (
+	GitJobSuccess GitJobResult = "Success"
+	GitJobFailure GitJobResult = "Failure"
+	GitJobUnknown GitJobResult = "Unknown"
+)
 
 // GitUpdateJobStatus defines the observed state of GitUpdateJob
 type GitUpdateJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// The StartTime and CompletionTime fields are intended to match
+	// batch/Job's status fields of the same name.
+
+	// StartTime gives the time the job started, if started.
+	// +optional
+	StartTime *metav1.Time `json:"startTime,omitempty"`
+	// FinishedTime gives the time the job finished running.
+	// +optional
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+
+	// batch/Job has a count of successful pods and failed pods, but I
+	// don't care about running several pods, so it's {success,
+	// failure, unknown}
+
+	// Result gives the outcome of the job, once completed.
+	// +optional
+	Result GitJobResult `json:"result,omitempty"`
+
+	// Error gives the error message, if the job ran into one.
+	// +optional
+	Error string `json:"error,omitempty"`
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Started",type=string,JSONPath=`.status.startedTime`
+// +kubebuilder:printcolumn:name="Finished",type=string,JSONPath=`.status.finishedTime`
+// +kubebuilder:printcolumn:name="Result",type=string,JSONPath=`.status.result`
 
 // GitUpdateJob is the Schema for the gitupdatejobs API
 type GitUpdateJob struct {
